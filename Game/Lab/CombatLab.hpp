30a155dd3be8bad979e8caa719ff7033fea85d98
hidden1 <hevele.balazs@gmail.com>
2018-09-30 12:30:45 +0300

    [Co1] Combat lab - enemy entities

--- Game/Lab/CombatLab.hpp
+++ Game/Lab/CombatLab.hpp
@@ -2,19 +2,26 @@
 
 #include <Windows.h>
 
+struct Entity
+{
+	V2 position;
+	V2 velocity;
+	F32 radius;
+
+	F32 health;
+	F32 maxHealth;
+};
+
+#define CombatLabEnemyN 10
 struct CombatLabState
 {
 	Camera camera;
 	Canvas canvas;
 	B32 running;
 
-	V2 playerPosition;
-	V2 playerVelocity;
+	Entity player;
+	Entity enemies[CombatLabEnemyN];
 
-	F32 health;
-	F32 maxHealth;
-
-	V2 circlePosition;
 	F32 circleRadius;
 	F32 circleTotalDuration;
 	F32 circleDurationLeft;
@@ -48,15 +55,17 @@ static void CombatLabBlit(Canvas canvas, HDC context, RECT rect)
 	);
 }
 
-static void DoDamage(CombatLabState* labState, F32 damage)
+static void DoDamage(Entity* entity, F32 damage)
 {
-	labState->health = Max2(labState->health - damage, 0.0f);
+	entity->health = Max2(entity->health - damage, 0.0f);
 }
 
 static LRESULT CALLBACK CombatLabCallback(HWND window, UINT message, WPARAM wparam, LPARAM lparam)
 {
-	CombatLabState* labState = &gCombatLabState;
 	LRESULT result = 0;
+
+	CombatLabState* labState = &gCombatLabState;
+	Entity* player = &labState->player;
 
 	switch (message)
 	{
@@ -95,21 +104,21 @@ static LRESULT CALLBACK CombatLabCallback(HWND window, UINT message, WPARAM wpar
 			{
 				case 'W':
 				{
-					labState->playerVelocity.y = -1.0f;
+					player->velocity.y = -1.0f;
 					break;
 				}
 				case 'S': {
-					labState->playerVelocity.y = +1.0f;
+					player->velocity.y = +1.0f;
 					break;
 				}
 				case 'A':
 				{
-					labState->playerVelocity.x = -1.0f;
+					player->velocity.x = -1.0f;
 					break;
 				}
 				case 'D':
 				{
-					labState->playerVelocity.x = +1.0f;
+					player->velocity.x = +1.0f;
 					break;
 				}
 			}
@@ -123,18 +132,18 @@ static LRESULT CALLBACK CombatLabCallback(HWND window, UINT message, WPARAM wpar
 				case 'W': 
 				case 'S':
 				{
-					labState->playerVelocity.y = 0.0f;
+					player->velocity.y = 0.0f;
 					break;
 				}
 				case 'A':
 				case 'D':
 				{
-					labState->playerVelocity.x = 0.0f;
+					player->velocity.x = 0.0f;
 					break;
 				}
 				case 'F':
 				{
-					DoDamage(labState, 20.0f);
+					DoDamage(player, 20.0f);
 					break;
 				}
 			}
@@ -157,10 +166,27 @@ static LRESULT CALLBACK CombatLabCallback(HWND window, UINT message, WPARAM wpar
 
 static void CombatLabInit(CombatLabState* labState, I32 windowWidth, I32 windowHeight)
 {
+	F32 maxHealth = 100.0f;
+
 	labState->running = true;
 
-	labState->maxHealth = 100.0f;
-	labState->health = labState->maxHealth;
+	Entity* player = &labState->player;
+	player->position = MakePoint(0.0f, 0.0f);
+	player->radius = 1.0f;
+
+	player->maxHealth = maxHealth;
+	player->health = maxHealth;
+
+	for (I32 i = 0; i < CombatLabEnemyN; ++i) 
+	{
+		Entity* enemy = &labState->enemies[i];
+		enemy->position.x = player->position.x + RandomBetween(-10.0f, +10.0f);
+		enemy->position.y = player->position.y + RandomBetween(-10.0f, +10.0f);
+		enemy->radius = 1.0f;
+
+		enemy->maxHealth = maxHealth;
+		enemy->health = maxHealth;
+	}
 
 	CombatLabResize(labState, windowWidth, windowHeight);
 }
@@ -206,10 +232,63 @@ static void DrawCircle(Canvas canvas, V2 center, F32 radius, V4 color)
 	}
 }
 
+static void DrawCircleBorder(Canvas canvas, V2 center, F32 radius, V4 color)
+{
+	I32 lineN = 20;
+	F32 angleAdvance = (2.0f * PI) / F32(lineN);
+	F32 angle = 0.0f;
+	for (I32 i = 0; i < lineN; ++i)
+	{
+		F32 angle1 = angle;
+		F32 angle2 = angle + angleAdvance;
+		V2 point1 = center + radius * RotationVector(angle1);
+		V2 point2 = center + radius * RotationVector(angle2);
+		Bresenham(canvas, point1, point2, color);
+
+		angle += angleAdvance;
+	}
+}
+
+static void DrawEntity(Canvas canvas, Entity* entity, V4 color)
+{
+	DrawCircle(canvas, entity->position, entity->radius, color);
+
+	V4 healthBarBackgroundColor = MakeColor(0.5f, 0.0f, 0.0f);
+	V4 healthBarColor = MakeColor(1.0f, 0.0f, 0.0f);
+	F32 healthBarWidth = 2.0f * entity->radius;
+	F32 healthBarHeight = 0.1f * healthBarWidth;
+
+	F32 healthBarX = entity->position.x;
+	F32 healthBarY = entity->position.y - entity->radius - healthBarHeight - healthBarHeight * 0.5f;
+
+	F32 healthBarLeft   = healthBarX - 0.5f * healthBarWidth;
+	F32 healthBarRight  = healthBarX + 0.5f * healthBarWidth;
+	F32 healthBarTop    = healthBarY - 0.5f * healthBarHeight;
+	F32 healthBarBottom = healthBarY + 0.5f * healthBarHeight;
+	DrawRect(canvas, healthBarLeft, healthBarRight, healthBarTop, healthBarBottom, healthBarBackgroundColor);
+
+	F32 healthRatio = entity->health / entity->maxHealth;
+	Assert(IsBetween(healthRatio, 0.0f, 1.0f));
+	F32 healthBarFilledX = healthBarLeft + healthRatio * (healthBarRight - healthBarLeft);
+	DrawRect(canvas, healthBarLeft, healthBarFilledX, healthBarTop, healthBarBottom, healthBarColor);
+}
+
+static void DamageEnemiesInCircle(CombatLabState* labState, V2 center, F32 radius, F32 damage)
+{
+	for (I32 i = 0; i < CombatLabEnemyN; ++i) 
+	{
+		Entity* enemy = &labState->enemies[i];
+		F32 enemyCircleDistance = Distance(enemy->position, center);
+		if (enemyCircleDistance < radius + enemy->radius)
+			DoDamage(enemy, damage);
+	}
+}
+
 static void CombatLabUpdate(CombatLabState* labState)
 {
 	F32 seconds = 0.05f;
-	labState->playerPosition = labState->playerPosition + seconds * labState->playerVelocity;
+	Entity* player = &labState->player;
+	player->position = player->position + seconds * player->velocity;
 
 	Canvas canvas = labState->canvas;
 	V4 backgroundColor = MakeColor(0.0f, 0.0f, 0.0f);
@@ -219,7 +298,7 @@ static void CombatLabUpdate(CombatLabState* labState)
 	F32 gridDistance = 10.0f;
 
 	Camera* camera = &labState->camera;
-	camera->center = labState->playerPosition;
+	camera->center = player->position;
 	F32 left   = CameraLeftSide(camera);
 	F32 right  = CameraRightSide(camera);
 	F32 top    = CameraTopSide(camera);
@@ -248,45 +327,27 @@ static void CombatLabUpdate(CombatLabState* labState)
 	labState->circleDurationLeft -= seconds;
 	if (labState->circleDurationLeft > 0.0f) 
 	{
-		F32 durationRatio = labState->circleDurationLeft / labState->circleTotalDuration;
-		V4 circleColor = MakeColor(1.0f, durationRatio, 0.0f);
-		DrawCircle(canvas, labState->circlePosition, labState->circleRadius, circleColor);
+		F32 durationRatio = 1.0f - labState->circleDurationLeft / labState->circleTotalDuration;
+		V4 circleColor = MakeColor(1.0f, 1.0f, 0.0f);
+		F32 fillRadius = labState->player.radius + (durationRatio) * (labState->circleRadius - labState->player.radius);
+		DrawCircleBorder(canvas, labState->player.position, labState->circleRadius, circleColor);
+		DrawCircle(canvas, labState->player.position, fillRadius, circleColor);
 	}
 	else
 	{
-		F32 playerCircleDistance = Distance(labState->playerPosition, labState->circlePosition);
-		if (playerCircleDistance < labState->circleRadius) 
-			DoDamage(labState, 10.0f);
+		DamageEnemiesInCircle(labState, labState->player.position, labState->circleRadius, 10.0f);
 
-		labState->circlePosition.x = RandomBetween(-10.0f, +10.0f);
-		labState->circlePosition.y = RandomBetween(-10.0f, +10.0f);
-		labState->circleRadius = RandomBetween(3.0f, 4.5f);
-		labState->circleTotalDuration = RandomBetween(20.0f, 30.0f);
+		labState->circleRadius = 3.0f;
+		labState->circleTotalDuration = 10.0f;
 		labState->circleDurationLeft = labState->circleTotalDuration;
 	}
 
-	F32 playerRadius = 1.0f;
 	V4 playerColor = MakeColor(1.0f, 1.0f, 1.0f);
-	DrawCircle(canvas, labState->playerPosition, playerRadius, playerColor);
+	DrawEntity(canvas, &labState->player, playerColor);
 
-	V4 healthBarBackgroundColor = MakeColor(0.5f, 0.0f, 0.0f);
-	V4 healthBarColor = MakeColor(1.0f, 0.0f, 0.0f);
-	F32 healthBarWidth = 2.0f * playerRadius;
-	F32 healthBarHeight = 0.1f * healthBarWidth;
-
-	F32 healthBarX = labState->playerPosition.x;
-	F32 healthBarY = labState->playerPosition.y - playerRadius - healthBarHeight - healthBarHeight * 0.5f;
-
-	F32 healthBarLeft   = healthBarX - 0.5f * healthBarWidth;
-	F32 healthBarRight  = healthBarX + 0.5f * healthBarWidth;
-	F32 healthBarTop    = healthBarY - 0.5f * healthBarHeight;
-	F32 healthBarBottom = healthBarY + 0.5f * healthBarHeight;
-	DrawRect(canvas, healthBarLeft, healthBarRight, healthBarTop, healthBarBottom, healthBarBackgroundColor);
-
-	F32 healthRatio = labState->health / labState->maxHealth;
-	Assert(IsBetween(healthRatio, 0.0f, 1.0f));
-	F32 healthBarFilledX = healthBarLeft + healthRatio * (healthBarRight - healthBarLeft);
-	DrawRect(canvas, healthBarLeft, healthBarFilledX, healthBarTop, healthBarBottom, healthBarColor);
+	V4 enemyColor = MakeColor(0.5f, 0.0f, 0.0f);
+	for (I32 i = 0; i < CombatLabEnemyN; i++)
+		DrawEntity(canvas, &labState->enemies[i], enemyColor);
 }
 
 static void CombatLab(HINSTANCE instance)
@@ -342,7 +403,10 @@ static void CombatLab(HINSTANCE instance)
 	}
 }
 
+// [TODO: Enemy circles that the player can damage!]
+	// [TODO: Move radius to Entity struct!]
+// TODO: Remove damage at start!
 // TODO: Real seconds!
-// TODO: Circles on the floor that deal damage!
 // TODO: Disable controls while dead!
+// TODO: Better controls (releasing/pressing buttons)!
 // TODO: Better physics?
