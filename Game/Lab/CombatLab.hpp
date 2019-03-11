af067dc92fb51001d4537bd3e4f83e9bfcd7ab78
hidden1 <hevele.balazs@gmail.com>
2019-03-11 07:43:11 +0200

    [Co9] effects and movement based abilities

--- Game/Lab/CombatLab.hpp
+++ Game/Lab/CombatLab.hpp
@@ -6,6 +6,26 @@
 #define TileRowN 10
 #define TileColN 10
 
+enum AbilityId
+{
+	NoAbilityId,
+	SmallPunchAbilityId,
+	BigPunchAbilityId,
+	KickAbilityId,
+	SpinningKickAbilityId,
+	AvoidanceAbilityId,
+	RollAbilityId,
+	AbilityN
+};
+
+enum EffectId
+{
+	NoEffectId,
+	KickedEffectId,
+	RollingEffectId,
+	InvulnerableEffectId
+};
+
 struct TileIndex
 {
 	I32 row;
@@ -17,27 +37,52 @@ struct TileIndex
 struct Entity
 {
 	V2 position;
+	V2 velocity;
 	F32 recharge;
 	F32 rechargeFrom;
 	I32 health;
+
+	V2 inputDirection;
 };
 
+struct Cooldown
+{
+	Entity* entity;
+	I32 abilityId;
+	F32 timeRemaining;
+};
+
+struct Effect
+{
+	Entity* entity;
+	I32 effectId;
+	F32 timeRemaining;
+};
+
+#define MaxCooldownN 64
+#define MaxEffectN 64
 struct CombatLabState
 {
 	Camera camera;
 	Canvas canvas;
 	B32 running;
 
-	V2 playerMoveDirection;
+	Cooldown cooldowns[MaxCooldownN];
+	I32 cooldownN;
+
+	Effect effects[MaxEffectN];
+	I32 effectN;
 
 	Entity player;
 	Entity enemy;
 };
 CombatLabState gCombatLabState;
 
-#define PlayerSpeed 20.0f
+#define PlayerSpeed 11.0f
 
 #define EnemyAttackRadius 10.0f
+
+#define MaxMeleeAttackDistance (4.0f * EntityRadius)
 
 static void func CombatLabResize(CombatLabState* labState, I32 width, I32 height)
 {
@@ -113,7 +158,7 @@ static void func CombatLabInit(CombatLabState* labState, I32 windowWidth, I32 wi
 	Entity* player = &labState->player;
 	player->position = mapCenter;
 	player->health = EntityMaxHealth;
-	labState->playerMoveDirection = MakePoint(0.0f, 0.0f);
+	player->inputDirection = MakePoint(0.0f, 0.0f);
 
 	Entity* enemy = &labState->enemy;
 	enemy->position = MakePoint(mapWidth - EntityRadius, mapHeight - EntityRadius);
@@ -132,11 +177,323 @@ static I32 func GetTileDistance(TileIndex tileIndex1, TileIndex tileIndex2)
 
 #define TileGridColor MakeColor(0.2f, 0.2f, 0.2f)
 
+static B32 func IsOnCooldown(CombatLabState* labState, Entity* entity, I32 abilityId)
+{
+	B32 isOnCooldown = false;
+	for(I32 i = 0; i < labState->cooldownN; i++)
+	{
+		Cooldown* cooldown = &labState->cooldowns[i];
+		if(cooldown->entity == entity && cooldown->abilityId == abilityId)
+		{
+			isOnCooldown = true;
+			break;
+		}
+	}
+	return isOnCooldown;
+}
+
+static B32 func IsDead(Entity* entity)
+{
+	B32 isDead = (entity->health == 0);
+	return isDead;
+}
+
+static B32 func CanUseAbility(CombatLabState* labState, I32 abilityId)
+{
+	B32 canUse = false;
+	Entity* player = &labState->player;
+	Entity* enemy = &labState->enemy;
+
+	if(IsDead(player) || player->recharge > 0.0f)
+	{
+		canUse = false;
+	}
+	else if(IsOnCooldown(labState, player, abilityId))
+	{
+		canUse = false;
+	}
+	else
+	{
+		switch(abilityId)
+		{
+			case SmallPunchAbilityId:
+			case BigPunchAbilityId:
+			case KickAbilityId:
+			{
+				canUse = (MaxDistance(player->position, enemy->position) <= MaxMeleeAttackDistance);
+				break;
+			}
+			case RollAbilityId:
+			{
+				canUse = (player->inputDirection.x != 0.0f || player->inputDirection.y != 0.0f);
+				break;
+			}
+			case AvoidanceAbilityId:
+			{
+				canUse = true;
+				break;
+			}
+			default:
+			{
+				DebugBreak();
+			}
+		}
+	}
+	return canUse;
+}
+
+static void func PutOnRecharge(Entity* entity, F32 rechargeTime)
+{
+	Assert(rechargeTime > 0.0f);
+	Assert(entity->recharge == 0.0f);
+	entity->rechargeFrom = rechargeTime;
+	entity->recharge = rechargeTime;
+}
+
+static B32 func HasEffect(CombatLabState* labState, Entity* entity, I32 effectId)
+{
+	B32 hasEffect = false;
+	for(I32 i = 0; i < labState->effectN; i++)
+	{
+		Effect* effect = &labState->effects[i];
+		if(effect->entity == entity && effect->effectId == effectId)
+		{
+			hasEffect = true;
+			break;
+		}
+	}
+	return hasEffect;
+}
+
+static B32 func CanDoDamage(CombatLabState* labState, Entity* entity)
+{
+	B32 canDoDamage = false;
+	if(IsDead(entity))
+	{
+		canDoDamage = false;
+	}
+	else if(HasEffect(labState, entity, InvulnerableEffectId))
+	{
+		canDoDamage = false;
+	}
+	else
+	{
+		canDoDamage = true;
+	}
+	return canDoDamage;
+}
+
+static void func DoDamage(Entity* entity, I32 damage)
+{
+	Assert(damage > 0);
+	entity->health = IntMax2(entity->health - damage, 0);
+}
+
+static void func AttemptToDoDamage(CombatLabState* labState, Entity* entity, I32 damage)
+{
+	if(CanDoDamage(labState, entity))
+	{
+		DoDamage(entity, damage);
+	}
+}
+
+static F32 func GetAbilityCooldownDuration(I32 abilityId)
+{
+	F32 duration = 0.0f;
+	switch(abilityId)
+	{
+		case BigPunchAbilityId:
+		{
+			duration = 3.0f;
+			break;
+		}
+		case KickAbilityId:
+		{
+			duration = 5.0f;
+			break;
+		}
+		case RollAbilityId:
+		{
+			duration = 5.0f;
+			break;
+		}
+		case AvoidanceAbilityId:
+		{
+			duration = 10.0f;
+			break;
+		}
+		default:
+		{
+			DebugBreak();
+		}
+	}
+	return duration;
+}
+
+static void func AddCooldown(CombatLabState* labState, Entity* entity, I32 abilityId)
+{
+	F32 duration = GetAbilityCooldownDuration(abilityId);
+
+	Assert(duration > 0.0f);
+	Assert(labState->cooldownN < MaxCooldownN);
+	Cooldown* cooldown = &labState->cooldowns[labState->cooldownN];
+	labState->cooldownN++;
+
+	cooldown->entity = entity;
+	cooldown->abilityId = abilityId;
+	cooldown->timeRemaining = duration;
+}
+
+static Cooldown* func GetCooldown(CombatLabState* labState, Entity* entity, I32 abilityId)
+{
+	Cooldown* result = 0;
+	for(I32 i = 0; i < labState->cooldownN; i++)
+	{
+		Cooldown* cooldown = &labState->cooldowns[i];
+		if(cooldown->entity == entity && cooldown->abilityId == abilityId)
+		{
+			result = cooldown;
+			break;
+		}
+	}
+	return result;
+}
+
+static V2 func GetClosestMoveDirection(V2 distance)
+{
+	V2 left  = MakeVector(-1.0f, 0.0f);
+	V2 right = MakeVector(+1.0f, 0.0f);
+	V2 up    = MakeVector(0.0f, -1.0f);
+	V2 down  = MakeVector(0.0f, +1.0f);
+	V2 direction = {};
+
+	if(distance.x > 0.0f)
+	{
+		if(distance.y < -2.0f * distance.x)
+		{
+			direction = up;
+		}
+		else if(distance.y < -0.5f * distance.x)
+		{
+			direction = up + right;
+		}
+		else if(distance.y < 0.5f * distance.x)
+		{
+			direction = right;
+		}
+		else if(distance.y < 2.0f * distance.x)
+		{
+			direction = down + right;
+		}
+		else
+		{
+			direction = down;
+		}
+	}
+	else
+	{
+		if(distance.y < 2.0f * distance.x)
+		{
+			direction = up;
+		}
+		else if(distance.y < 0.5f * distance.x)
+		{
+			direction = up + left;
+		}
+		else if(distance.y < -0.5f * distance.x)
+		{
+			direction = left;
+		}
+		else if(distance.y < -2.0f * distance.x)
+		{
+			direction = down + left;
+		}
+		else
+		{
+			direction = down;
+		}
+	}
+	return direction;
+}
+
+static void func AddEffect(CombatLabState* labState, Entity* entity, I32 effectId, F32 duration)
+{
+	Assert(labState->effectN < MaxEffectN);
+	Effect* effect = &labState->effects[labState->effectN];
+	labState->effectN++;
+
+	effect->entity = entity;
+	effect->effectId = effectId;
+	Assert(duration > 0.0f);
+	effect->timeRemaining = duration;
+}
+
+static void func UseAbility(CombatLabState* labState, I32 abilityId)
+{
+	Assert(CanUseAbility(labState, abilityId));
+	Entity* player = &labState->player;
+	Entity* enemy = &labState->enemy;
+	switch (abilityId)
+	{
+		case SmallPunchAbilityId:
+		{
+			AttemptToDoDamage(labState, enemy, 10);
+			PutOnRecharge(player, 1.0f);
+			break;
+		}
+		case BigPunchAbilityId:
+		{
+			AttemptToDoDamage(labState, enemy, 30);
+			PutOnRecharge(player, 1.0f);
+			AddCooldown(labState, player, BigPunchAbilityId);
+			break;
+		}
+		case KickAbilityId:
+		{
+			F32 moveSpeed = 20.0f;
+			enemy->velocity = moveSpeed * GetClosestMoveDirection(enemy->position - player->position);
+			AddEffect(labState, enemy, KickedEffectId, 1.0f);
+			PutOnRecharge(player, 1.0f);
+			AddCooldown(labState, player, KickAbilityId);
+			break;
+		}
+		case RollAbilityId:
+		{
+			F32 moveSpeed = 20.0f;
+			player->velocity = moveSpeed * GetClosestMoveDirection(player->inputDirection);
+			AddEffect(labState, player, RollingEffectId, 1.0f);
+			PutOnRecharge(player, 1.0f);
+			AddCooldown(labState, player, RollAbilityId);
+			break;
+		}
+		case AvoidanceAbilityId:
+		{
+			AddEffect(labState, player, InvulnerableEffectId, 2.0f);
+			PutOnRecharge(player, 2.0f);
+			AddCooldown(labState, player, AvoidanceAbilityId);
+			break;
+		}
+		default:
+		{
+			DebugBreak();
+		}
+	}
+}
+
+static void func AttemptToUseAbility(CombatLabState* labState, I32 abilityId)
+{
+	if(CanUseAbility(labState, abilityId))
+	{
+		UseAbility(labState, abilityId);
+	}
+}
+
 static LRESULT CALLBACK func CombatLabCallback(HWND window, UINT message, WPARAM wparam, LPARAM lparam)
 {
 	LRESULT result = 0;
 	
 	CombatLabState* labState = &gCombatLabState;
+	Entity* player = &labState->player;
 	switch(message)
 	{
 		case WM_SIZE:
@@ -166,24 +523,53 @@ static LRESULT CALLBACK func CombatLabCallback(HWND window, UINT message, WPARAM
 			WPARAM keyCode = wparam;
 			switch(keyCode)
 			{
+				case VK_LEFT:
 				case 'A':
 				{
-					labState->playerMoveDirection.x = -1.0f;
+					player->inputDirection.x = -1.0f;
 					break;
 				}
+				case VK_RIGHT:
 				case 'D':
 				{
-					labState->playerMoveDirection.x = +1.0f;
+					player->inputDirection.x = +1.0f;
 					break;
 				}
+				case VK_UP:
 				case 'W':
 				{
-					labState->playerMoveDirection.y = -1.0f;
+					player->inputDirection.y = -1.0f;
 					break;
 				}
+				case VK_DOWN:
 				case 'S':
 				{
-					labState->playerMoveDirection.y = +1.0f;
+					player->inputDirection.y = +1.0f;
+					break;
+				}
+				case '1':
+				{
+					AttemptToUseAbility(labState, SmallPunchAbilityId);
+					break;
+				}
+				case '2':
+				{
+					AttemptToUseAbility(labState, BigPunchAbilityId);
+					break;
+				}
+				case '3':
+				{
+					AttemptToUseAbility(labState, KickAbilityId);
+					break;
+				}
+				case '4':
+				{
+					AttemptToUseAbility(labState, RollAbilityId);
+					break;
+				}
+				case '5':
+				{
+					AttemptToUseAbility(labState, AvoidanceAbilityId);
 					break;
 				}
 			}
@@ -194,16 +580,20 @@ static LRESULT CALLBACK func CombatLabCallback(HWND window, UINT message, WPARAM
 			WPARAM keyCode = wparam;
 			switch(keyCode) 
 			{
+				case VK_LEFT:
 				case 'A':
+				case VK_RIGHT:
 				case 'D':
 				{
-					labState->playerMoveDirection.x = 0.0f;
+					player->inputDirection.x = 0.0f;
 					break;
 				}
+				case VK_UP:
 				case 'W':
+				case VK_DOWN:
 				case 'S':
 				{
-					labState->playerMoveDirection.y = 0.0f;
+					player->inputDirection.y = 0.0f;
 					break;
 				}
 			}
@@ -337,6 +727,146 @@ static void func DrawEntityBars(Canvas* canvas, Entity* entity)
 	}
 }
 
+static void func DrawAbilityBar(CombatLabState* labState)
+{
+	Bitmap* bitmap = &labState->canvas.bitmap;
+	GlyphData* glyphData = labState->canvas.glyphData;
+	Assert(glyphData != 0);
+
+	Camera* camera = &labState->camera;
+	I32 abilityIds[5] = 
+	{
+		SmallPunchAbilityId,
+		BigPunchAbilityId,
+		KickAbilityId,
+		RollAbilityId,
+		AvoidanceAbilityId
+	};
+	I32 abilityN = 5;
+
+	I32 boxSide = 40;
+	I32 padding = 5;
+
+	I32 centerX = (bitmap->width - 1) / 2;
+	I32 width = boxSide * abilityN + padding * (abilityN - 1);
+
+	I32 left = centerX - width / 2;
+	I32 right = centerX + width / 2;
+	I32 bottom = (bitmap->height - 1) - 30;
+	I32 top = bottom - boxSide;
+
+	Entity* entity = &labState->player;
+
+	I32 boxLeft = left;
+	for(I32 i = 0; i < abilityN; i++)
+	{
+		I32 abilityId = abilityIds[i];
+
+		I32 boxRight = boxLeft + boxSide;
+		I32 boxTop = top;
+		I32 boxBottom = bottom;
+
+		V4 boxOutlineColor = MakeColor(1.0f, 1.0f, 1.0f);
+		V4 boxBackgroundColor = MakeColor(0.0f, 0.0f, 0.0f);
+		V4 boxCannotUseColor = MakeColor(0.2f, 0.0f, 0.0f);
+		V4 textColor = MakeColor(1.0f, 1.0f, 1.0f);
+		DrawBitmapRect(bitmap, boxLeft, boxRight, boxTop, boxBottom, boxBackgroundColor);
+
+		F32 recharge = 0.0f;
+		F32 rechargeFrom = 0.0f;
+		
+		Cooldown* cooldown = GetCooldown(labState, entity, abilityId);
+		if(cooldown)
+		{
+			recharge = cooldown->timeRemaining;
+			rechargeFrom = GetAbilityCooldownDuration(abilityId);
+		}
+		else if (entity->recharge > 0.0f)
+		{
+			recharge = entity->recharge;
+			rechargeFrom = entity->rechargeFrom;
+		}
+		else if(!CanUseAbility(labState, abilityId))
+		{
+			DrawBitmapRect(bitmap, boxLeft, boxRight, boxTop, boxBottom, boxCannotUseColor);
+		}
+
+		V4 rechargeColor = MakeColor(0.5f, 0.5f, 0.5f);
+		if(rechargeFrom > 0.0f)
+		{
+			F32 rechargeRatio = (recharge / rechargeFrom);
+			Assert(IsBetween(rechargeRatio, 0.0f, 1.0f));
+			I32 rechargeRight = (I32)Lerp((F32)boxLeft, rechargeRatio, (F32)boxRight);
+			DrawBitmapRect(bitmap, boxLeft, rechargeRight, boxTop, boxBottom, rechargeColor);
+		}
+
+		I8 name[8] = {};
+		OneLineString(name, 8, (i + 1));
+		DrawBitmapTextLineCentered(bitmap, name, glyphData,
+								   boxLeft, boxRight, boxTop, boxBottom,
+								   textColor);
+
+		DrawBitmapRectOutline(bitmap, boxLeft, boxRight, boxTop, boxBottom, boxOutlineColor);
+
+		boxLeft = boxRight + padding;
+	}
+}
+
+static void func UpdateEntityRecharge(Entity* entity, F32 seconds)
+{
+	entity->recharge = Max2(entity->recharge - seconds, 0.0f);
+}
+
+static void func UpdateCooldowns(CombatLabState* labState, F32 seconds)
+{
+	I32 remainingCooldownN = 0;
+	for(I32 i = 0; i < labState->cooldownN; i++)
+	{
+		Cooldown* cooldown = &labState->cooldowns[i];
+		cooldown->timeRemaining -= seconds;
+		if(cooldown->timeRemaining > 0.0f)
+		{
+			labState->cooldowns[remainingCooldownN] = *cooldown;
+			remainingCooldownN++;
+		}
+	}
+	labState->cooldownN = remainingCooldownN;
+}
+
+static void func UpdateEffects(CombatLabState* labState, F32 seconds)
+{
+	I32 remainingEffectN = 0;
+	for(I32 i = 0; i < labState->effectN; i++)
+	{
+		Effect* effect = &labState->effects[i];
+		effect->timeRemaining -= seconds;
+		if(effect->timeRemaining > 0.0f)
+		{
+			labState->effects[remainingEffectN] = *effect;
+			remainingEffectN++;
+		}
+	}
+	labState->effectN = remainingEffectN;
+}
+
+static B32 func CanMove(CombatLabState* labState, Entity* entity)
+{
+	B32 canMove = false;
+	if(IsDead(entity))
+	{
+		canMove = false;
+	}
+	else if(HasEffect(labState, entity, KickedEffectId) || HasEffect(labState, entity, RollingEffectId))
+	{
+		canMove = false;
+	}
+	else
+	{
+		canMove = true;
+	}
+	return canMove;
+}
+
 static void func CombatLabUpdate(CombatLabState* labState, V2 mousePosition, F32 seconds)
 {
 	Canvas* canvas = &labState->canvas;
@@ -371,11 +901,20 @@ static void func CombatLabUpdate(CombatLabState* labState, V2 mousePosition, F32
 	Entity* player = &labState->player;
 	Entity* enemy = &labState->enemy;
 
-	player->position = player->position + PlayerSpeed * seconds * labState->playerMoveDirection;
+	UpdateCooldowns(labState, seconds);
+	UpdateEffects(labState, seconds);
 
+	if(IsDead(player))
+	{
+		player->velocity = MakeVector(0.0f, 0.0f);
+	}
+	if(CanMove(labState, player))
+	{
+		player->velocity = PlayerSpeed * player->inputDirection;
+	}
+	player->position = player->position + seconds * player->velocity;
 	player->position.x = Clip(player->position.x, EntityRadius, mapWidth - EntityRadius);
 	player->position.y = Clip(player->position.y, EntityRadius, mapHeight - EntityRadius);
-
 
 	V4 playerColor = MakeColor(0.0f, 1.0f, 1.0f);
 	DrawEntity(canvas, player, playerColor);
@@ -389,7 +928,6 @@ static void func CombatLabUpdate(CombatLabState* labState, V2 mousePosition, F32
 	V2 targetDirection = {};
 	V2 targetPosition = {};
 	TileIndex playerTileIndex = GetContainingTileIndex(player->position);
-	F32 attackDistance = 2.5f * EntityRadius;
 
 	for(I32 row = playerTileIndex.row - 1; row <= playerTileIndex.row + 1; row++)
 	{
@@ -405,6 +943,7 @@ static void func CombatLabUpdate(CombatLabState* labState, V2 mousePosition, F32
 				continue;
 			}
 
+			F32 attackDistance = 2.5f * EntityRadius;
 			TileIndex tileIndex = MakeTileIndex(row, col);
 			if(IsValidTileIndex(tileIndex))
 			{
@@ -424,36 +963,45 @@ static void func CombatLabUpdate(CombatLabState* labState, V2 mousePosition, F32
 
 	Assert(foundTargetTile);
 	F32 enemyMoveSpeed = 10.0f;
-	F32 enemyMoveDistance = seconds * enemyMoveSpeed;
 
-	if(enemy->position.x < targetPosition.x)
+	if(IsDead(enemy))
 	{
-		enemy->position.x = Min2(enemy->position.x + enemyMoveDistance, targetPosition.x);
+		enemy->velocity = MakeVector(0.0f, 0.0f);
 	}
-	else
+	if(CanMove(labState, enemy))
 	{
-		enemy->position.x = Max2(enemy->position.x - enemyMoveDistance, targetPosition.x);
-	}
+		enemy->velocity = MakeVector(0.0f, 0.0f);
+		if(enemy->position.x < targetPosition.x)
+		{
+			enemy->velocity.x = Min2(+enemyMoveSpeed, (targetPosition.x - enemy->position.x) / seconds);
+		}
+		else
+		{
+			enemy->velocity.x = Max2(-enemyMoveSpeed, (targetPosition.x - enemy->position.x) / seconds);
+		}
 
-	if(enemy->position.y < targetPosition.y)
-	{
-		enemy->position.y = Min2(enemy->position.y + enemyMoveDistance, targetPosition.y);
+		if(enemy->position.y < targetPosition.y)
+		{
+			enemy->velocity.y = Min2(+enemyMoveSpeed, (targetPosition.y - enemy->position.y) / seconds);
+		}
+		else
+		{
+			enemy->velocity.y = Max2(-enemyMoveSpeed, (targetPosition.y - enemy->position.y) / seconds);
+		}
 	}
-	else
-	{
-		enemy->position.y = Max2(enemy->position.y - enemyMoveDistance, targetPosition.y);
-	}
+	enemy->position = enemy->position + seconds * enemy->velocity;
+	enemy->position.x = Clip(enemy->position.x, EntityRadius, mapWidth - EntityRadius);
+	enemy->position.y = Clip(enemy->position.y, EntityRadius, mapHeight - EntityRadius);
 
-	enemy->recharge = Max2(enemy->recharge - seconds, 0.0f);
+	UpdateEntityRecharge(player, seconds);
+	UpdateEntityRecharge(enemy, seconds);
 
 	if(MaxDistance(enemy->position, player->position) <= EnemyAttackRadius)
 	{
 		if(enemy->recharge == 0.0f)
 		{
-			player->health = IntMax2(player->health - 10, 0);
-
-			enemy->rechargeFrom = 3.0f;
-			enemy->recharge = enemy->rechargeFrom;
+			AttemptToDoDamage(labState, player, 10);
+			PutOnRecharge(enemy, 3.0f);
 		}
 	}
 
@@ -463,6 +1011,8 @@ static void func CombatLabUpdate(CombatLabState* labState, V2 mousePosition, F32
 	V2 mapCenter = MakePoint(mapWidth * 0.5f, mapHeight * 0.5f);
 	Camera* camera = &labState->camera;
 	camera->center = mapCenter;
+
+	DrawAbilityBar(labState);
 }
 
 static void func CombatLab(HINSTANCE instance)
@@ -532,7 +1082,11 @@ static void func CombatLab(HINSTANCE instance)
 	}
 }
 
-// TODO: Real time Monk abilities!
+// TODO: Real time Monk abilities
+// TODO: Momentum
+// TODO: Enemy ability
+// TODO: Disable entity on death!
+	// TODO: Stop enemy from using abilities!
 // TODO: Computer controlled enemies
 	// TODO: Only get pulled from a certain distance!
 	// TODO: Multiple enemies
